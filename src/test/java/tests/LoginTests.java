94c9d1e5a75f0bd048aade61a3f96d9bc9ea77fc
milutinacimovic93 <milutinacimovic93@gmail.com>
2022-10-04 20:31:16 +0200

    Added third test verifying that using email and password with faker for non existing user will display error message, and also that URL contains login route.

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,21 +1,20 @@
 package tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.BasePage;
 import pages.HomePage;
 import pages.LoginPage;
-
 public class LoginTests extends BaseTest{
 
     private LoginPage loginPage;
     private BasePage basePage;
     private HomePage homePage;
+    private Faker faker;
 
 
     @BeforeClass
@@ -42,6 +41,24 @@ public class LoginTests extends BaseTest{
         String actualPass = loginPage.getPasswordField().getAttribute("type");
         Assert.assertEquals(actualEmail, "email");
         Assert.assertEquals(actualPass, "password");
+    }
+
+    @Test(priority = 3)
+    public void noUserError() throws InterruptedException {
+        faker = new Faker();
+        homePage.goToLoginPage();
+        String email = faker.internet().emailAddress();
+        String password = faker.internet().password();
+        loginPage.getEmailField().click();
+        loginPage.getEmailField().sendKeys(email);
+        loginPage.getPasswordField().click();
+        loginPage.getPasswordField().sendKeys(password);
+        loginPage.getLoginBtn().click();
+        Thread.sleep(3000);
+        WebElement userDoesNotExist = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/main/div/div[2]/div/div/div[4]/div/div/div/div/div[1]"));
+        Assert.assertTrue(userDoesNotExist.isDisplayed());
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains("login"));
     }
 
 }
